43443533e053212d5aac2296a5a29c2484c60276
Hoang1001 <duchoang.embedded@gmail.com>
2024-01-12 12:47:40 +0700

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+//Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+//Development mode
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//Container
+export { mount };
